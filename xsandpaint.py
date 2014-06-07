b6fb4db19c54fbb017df64801dbf247b3ddc9579
anders hoff <shall.be.lifted@gmail.com>
2014-06-07 17:51:59 +0200

    .

--- xsandpaint.py
+++ xsandpaint.py
@@ -7,18 +7,42 @@ import threading
 import cairo, Image
 import numpy as np
 from json import loads,dumps
-from numpy.random import random
+
+from numpy import sin, cos, pi, arctan2, square,sqrt, logical_not,\
+                  linspace, array, zeros
+from numpy.random import random, randint, shuffle
+
+
+#COLOR_PATH = 'color/dark_cyan_white_black.gif'
 
 PORT = 6379
 HOST = 'localhost'
 
 SIZE = 1000
-PIX = 1./SIZE
-
-BACK = [1.]*3
-FRONT = [0.5,0.1,0.1,1]
+SIZE = 20000 # size of png image
 
 DRAW_ITT = 10000
+
+PI = pi
+TWOPI = pi*2.
+
+NUM = 500 # number of nodes
+MAXFS = 10 # max friendships pr node
+
+BACK = 1. # background color 
+GRAINS = 40
+ALPHA = 0.05 # opacity of drawn points
+STEPS = 10**7
+
+ONE = 1./SIZE
+STP = ONE/15.
+
+RAD = 0.26 # radius of starting circle
+FARL  = 0.13 # ignore "enemies" beyond this radius
+NEARL = 0.02 # do not attempt to approach friends close than this
+
+FRIENDSHIP_RATIO = 0.1 # probability of friendship dens
+FRIENDSHIP_INITIATE_PROB = 0.05 # probability of friendship initation attempt
 
 class Render(object):
 
@@ -42,22 +66,23 @@ class Render(object):
 
   def dot(self,x,y):
 
-    self.ctx.rectangle(x,y,PIX,PIX)
+    self.ctx.rectangle(x,y,ONE,ONE)
     self.ctx.fill()
 
  
-class Listener(threading.Thread):
+class XRender(threading.Thread):
 
-  def __init__(self, r, channels, render):
+  def __init__(self, r, channel, path):
     threading.Thread.__init__(self)
-    
-    self.itt = 0
-    self.file_name = './test_{:08d}.png'
 
-    self.render = render
+    self.filename = path+'/test_{:08d}_q{:s}.png'
+    self.channel = channel
+
+    self.render = Render()
+    
     self.redis = r
     self.pubsub = self.redis.pubsub()
-    self.pubsub.subscribe(channels)
+    self.pubsub.subscribe(channel)
   
   def work(self, item):
   
@@ -65,9 +90,9 @@ class Listener(threading.Thread):
     self.render.dot(d['x'],d['y'])
 
     if not d['i'] % DRAW_ITT:
-      fname = self.file_name.format(d['i'])
-      print fname
-      self.render.sur.write_to_png(fname)
+      fn = self.filename.format(d['i'],self.channel)
+      print self,'writing:',fn
+      self.render.sur.write_to_png(fn)
   
   def run(self):
 
@@ -85,13 +110,16 @@ if __name__ == "__main__":
 
   r = redis.Redis(host=HOST,port=PORT)
 
-  client = Listener(r,['test'],Render())
-  client.start()
+  xrender = XRender(r,'1','./img/')
+  xrender.start()
 
-  for i in xrange(100000):
-    x = random()
-    y = random()
-    r.publish('test', dumps({'x':x,'y':y,'i':i}))
- 
-  r.publish('test', 'KILL')
+  try:
+    for i in xrange(100000):
+      x = random()
+      y = random()
+      r.publish('1', dumps({'x':x,'y':y,'i':i}))
+  except:
+    pass
+  finally:
+    r.publish('1', 'KILL')
 
